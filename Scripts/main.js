ef91ad7577034b7cbb336325362d32ff297fc0a8
zobad16 <zobad.mahmood@gmail.com>
2020-09-20 09:47:19 +0300

    pattern based ar

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,8 +3,8 @@ function GenerateQR()
    let link = "https://romantic-euclid-c8f58b.netlify.app/ar.html";
    let qrContainer = new QRCode(document.getElementById("qr-container"),{
       logo: "./asset/logo.png",
-      logoWidth: undefined,
-      logoHeight: undefined,
+      logoWidth: 128,
+      logoHeight: 128,
       width: 128,
 	   height: 128,
       logoBackgroundColor: '#ffffff',
