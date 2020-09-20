f74296f18cf823dda893b90065223ad4bf316116
zobad16 <zobad.mahmood@gmail.com>
2020-09-20 09:31:18 +0300

    qr code support

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,16 @@
 function GenerateQR()
 {
    let link = "https://romantic-euclid-c8f58b.netlify.app/ar.html";
-   let qrContainer = new QRCode(document.getElementById("qr-container")) ;
+   let qrContainer = new QRCode(document.getElementById("qr-container"),{
+      logo: "./asset/logo.png",
+      logoWidth: undefined,
+      logoHeight: undefined,
+      width: 128,
+	   height: 128,
+      logoBackgroundColor: '#ffffff',
+      logoBackgroundTransparent: false
+
+   }) ;
    qrContainer.makeCode(link);
 }
 GenerateQR();
